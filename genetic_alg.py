c53c7294f8d237a9263bff5fbcd7eca46f2785fb
Andrew-Atwater <andrew.atwater@maine.edu>
2025-04-30 14:06:32 -0400

    currently works but does not stop

--- genetic_alg.py
+++ genetic_alg.py
@@ -1,13 +1,33 @@
 import string
 import random
 
-possibleCharacters = string.ascii_letters + string.ascii_digits + ' .,?!;:\'"'
+possibleCharacters = string.ascii_letters + string.digits + ' .,?!;:\'"'
 
-t = "test"
+with open('lotr.txt', 'r') as file:
+    t = file.read()
 
 def generate_string(target):
     length = len(target)
-    possibleCharacters = string.ascii_letters + string.ascii_digits + ' .,?!;:\'"'
-    random = ''.join(random.choice(possibleCharacters) for _ in range(length))
-    return random
+    randomtest = ''.join(random.choice(possibleCharacters) for _ in range(length))
+    return randomtest
 
+def fitness(curAttempt):
+    return sum(1 for a, b in zip(curAttempt, t) if a == b)
+
+def mutate(parent):
+    index = random.randint(0, len(parent) - 1)
+    child = list(parent)
+    child[index] = random.choice(possibleCharacters)
+    return ''.join(child)
+
+attempt = generate_string(t)
+iteration = 0
+
+while attempt != t:
+    print(attempt)
+    new = mutate(attempt)
+    if fitness(new) >= fitness(attempt):
+        attempt = new
+    iteration += 1
+print(attempt)
+print(f"Target reached: {iteration} iterations.")
